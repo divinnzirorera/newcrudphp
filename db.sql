1425451fbe325bd4324b331838acc7090fdd4a0b
divinnzirorera <118374661+divinnzirorera@users.noreply.github.com>
2023-05-08 21:51:40 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@ CREATE DATABASE STUDENT_DB;
 USE STUDENT_DB;
 
 CREATE TABLE User(
-    id int(11)  PRIMARY KEY,
+    id int(11)   AUTO_INCREMENT PRIMARY KEY,
     fname varchar(255) not null,
     lname varchar(255) not null,
     password varchar(255) not null,
